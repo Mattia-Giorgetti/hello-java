d636c14b4cdd8984f8dc9fa86aea5beb8cfd658e
Mattia-Giorgetti <mattiagiorgetti37@gmail.com>
2023-03-07 15:34:42 +0100

    Add some variables

--- src/org/lessons/java/StampaSaluto.java
+++ src/org/lessons/java/StampaSaluto.java
@@ -2,6 +2,12 @@ package org.lessons.java;
 
 public class StampaSaluto {
     public static void main(String[] args) {
+        String name = "Mattia";
+        String surname = "Giorgetti";
+        String fullname = name + " " + surname;
+        int age = 32;
         System.out.println("Hello Java!");
+        System.out.println("My name is" + " " + fullname);
+        System.out.printf("I'm %s years old", age);
     }
 }
